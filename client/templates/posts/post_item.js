c99661cf36f9de03e500c930b4c491452da8397f
wynnyr <33757411+wynnyr@users.noreply.github.com>
2018-03-16 09:42:07 +0700

    AddPostsLimit

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,5 +6,5 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
-  }
+  },
 });
--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,17 @@
-Meteor.publish('posts', function() { 
-    return Posts.find(); 
+Meteor.publish('posts', function(options) {
+    check(options, {
+      sort: Object,
+      limit: Number
+    });
+    return Posts.find({}, options);
 });
 
-Meteor.publish('comments', function() {
-    return Comments.find();
+Meteor.publish('singlePost', function(id) {
+    check(id, String)
+    return Posts.find(id);
+});
+
+Meteor.publish('comments', function(postId) {
+    check(postId, String)
+    return Comments.find({postId: postId});
 });
